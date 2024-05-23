b40f34bfd9251ee0587b05c4c0e7cffc48e07c10
Shawn LoPorto <sloporto@asi-test.com>
2024-05-23 09:45:24 -0600

    More cleanup

--- buttonInteraction.py
+++ buttonInteraction.py
@@ -2,16 +2,9 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 
 class TestClickingButtons():
-#   def setup_method(self, method):
   def __init__(self):
-    # self.driver = webdriver.Firefox()
     options = webdriver.FirefoxOptions()
     self.driver = webdriver.Firefox(options=options)
-    # self.vars = {}
-
-#   def test_basic_options():
-#     options = webdriver.FirefoxOptions()
-#     driver = webdriver.Firefox(options=options)
 
   def teardown_method(self, method):
     self.driver.quit()
