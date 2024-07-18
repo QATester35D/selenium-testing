5da85d421e9df556fe732361b572eb678456c2e3
Shawn LoPorto <sloporto@asi-test.com>
2024-07-17 21:17:59 -0600

    Adding the web form automation

--- temp.py
+++ temp.py
@@ -3,21 +3,19 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
+from datetime import timedelta, date
 import time
 
+#########################################################################
+#Test Case to fill out form
+#########################################################################
 formybrowser = webdriver.Firefox()
-print("Now working with the Formy webpages - the \"Radio Button\" page")
+print("Now working with the Formy webpages - the \"Complete Web Form\" page")
 formybrowser.get("https://formy-project.herokuapp.com/")
-formybrowser.set_window_size(800, 500)
-keyMousePress=formybrowser.find_element(By.LINK_TEXT, "Radio Button")
+formybrowser.set_window_size(800, 800)
+keyMousePress=formybrowser.find_element(By.LINK_TEXT, "Complete Web Form")
 keyMousePress.click()
 
-element= WebDriverWait(formybrowser, 10).until(EC.presence_of_element_located((By.ID, "radio-button-1")))
+element= WebDriverWait(formybrowser, 10).until(EC.presence_of_element_located((By.ID, "datepicker")))
 
-#########################################################################
-#new code
-#########################################################################
-radioButtonList=formybrowser.find_elements(By.NAME, "exampleRadios") #The actual radio button circle itself
-radioButtonLabelList=formybrowser.find_elements(By.CLASS_NAME, "form-check-label") #The label associated with each radio button
-
-time.sleep(2) #Used only for testing purposes to see what is on the screen before the window is closed
+firstName=formybrowser.find_element(By.ID, "first-name") #text field
---
+++ webForm.py
@@ -0,0 +1,115 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.keys import Keys
+from datetime import timedelta, date
+import time
+
+#########################################################################
+#Setting up a tuple to make this test script a data driven test
+#########################################################################
+formTestData = [("Jerry","Seinfeld","Comedian","Grad","Male","4","+40"),
+    ("Cosmos","Kramer","Schmooch","HighSchool","NotSay","1","0"),
+    ("George","Costanza","Yankees Executive","College","Male","2","+20"),
+    ("Elaine","Benes","Copy Editor","Grad","Female","1","-12"),
+    ("Estelle","Costanza","Mom","HighSchool","Female","4","+42"),
+    ("Francis","Newman","Postman","HighSchool","NotSay","3","-24"),
+    ("Helen","Seinfeld","Mom","Grad","Female","2","-2")
+]
+
+def findDate (dateValue):
+    # -x = subtract number from today's date, 0 = today's date, +x = add number to today's date
+    dateConversion=len(dateValue)
+    if dateConversion == 1:
+        dateManipulation = "today"
+    else:
+        symbolForDate=dateValue[:1]
+        if symbolForDate == "+":
+            dateManipulation = "add"
+        elif symbolForDate == "-":
+            dateManipulation = "sub"
+        digitForDate=int(dateValue[1:])
+
+    match dateManipulation:
+        case "today":
+            newDate=1
+        case "add":
+            addToDate = date.today() + timedelta(digitForDate)
+            newDate=str(addToDate.month) +"/"+ str(addToDate.day) +"/"+ str(addToDate.year)
+        case "sub":
+            subFromDate = date.today() - timedelta(digitForDate)
+            newDate=str(addToDate.month) +"/"+ str(addToDate.day) +"/"+ str(addToDate.year)
+        case _:
+            print("Something went wrong with converting date.")
+    
+    return newDate
+
+#########################################################################
+#Test Case to fill out form
+#########################################################################
+formybrowser = webdriver.Firefox()
+print("Now working with the Formy webpages - the \"Complete Web Form\" page")
+formybrowser.get("https://formy-project.herokuapp.com/")
+formybrowser.set_window_size(800, 800)
+keyMousePress=formybrowser.find_element(By.LINK_TEXT, "Complete Web Form")
+keyMousePress.click()
+
+element= WebDriverWait(formybrowser, 10).until(EC.presence_of_element_located((By.ID, "datepicker")))
+
+firstName=formybrowser.find_element(By.ID, "first-name") #text field
+lastName=formybrowser.find_element(By.ID, "last-name") #text field
+jobTitle=formybrowser.find_element(By.ID, "job-title") #text field
+edRb1HighSchool=formybrowser.find_element(By.ID, "radio-button-1") #radio button
+edRb2College=formybrowser.find_element(By.ID, "radio-button-2") #radio button
+edRb3Grad=formybrowser.find_element(By.ID, "radio-button-3") #radio button
+sexMale=formybrowser.find_element(By.ID, "checkbox-1") #checkbox
+sexFemale=formybrowser.find_element(By.ID, "checkbox-2") #checkbox
+sexNotSay=formybrowser.find_element(By.ID, "checkbox-3") #checkbox
+yrsExp=formybrowser.find_element(By.ID, "select-menu") #dropbox
+someDate=formybrowser.find_element(By.ID, "datepicker") #datepicker
+
+#get tuple size
+for sublist in formTestData:
+    #("Jerry","Seinfeld","Comedian","Grad","Male","4","+40")
+    firstName.send_keys(sublist[0])
+    lastName.send_keys(sublist[1])
+    jobTitle.send_keys(sublist[2])
+ 
+    #Education
+    match sublist[3]:
+        case "HighSchool":
+            edRb1HighSchool.click()
+        case "College":
+            edRb2College.click()
+        case "Grad":
+            edRb3Grad.click()
+        case _:
+            print("The data value isn't a valid one for education.")
+#("Jerry","Seinfeld","Comedian","Grad","Male","4","+40")
+    #Sex
+    match sublist[4]:
+        case "Male":
+            sexMale.click()
+        case "Female":
+            sexFemale.click()
+        case "NotSay":
+            sexNotSay.click()
+        case _:
+            print("The data value isn't a valid one for education.")
+
+    #Years of experience
+    # yrsExp.select_by_index(sublist[5])
+    formybrowser.find_element(By.ID, "select-menu")
+    
+
+    #Date
+    testData=formTestData[0]
+    dateToSelect=findDate(testData[6])
+    if dateToSelect == "1":
+        someDate.send_keys(Keys.ENTER)
+    else:
+        someDate.send_keys(dateToSelect)
+        someDate.send_keys(Keys.TAB)
+
+    time.sleep(2) #Used only for testing purposes to see what is on the screen before the window is closed
